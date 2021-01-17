b720fa976b8e1551a66e2bccc72b023f3472bdb7
tara-s-r <44852921+tara-s-r@users.noreply.github.com>
2021-01-17 03:51:02 -0600

    some more solutions, still need to finish one up

---
+++ fib.js
@@ -0,0 +1,34 @@
+//return the nth fibonacci number, index starts at 0
+function fib(n) {
+    var a = 0
+    var b = 1
+    for (let i = 1; i <= n; i++) {
+        var tot = a+b
+        b = a
+        a = tot
+    }
+    return a
+}
+
+//tests
+console.log(fib(0)) // 0
+console.log(fib(1)) // 1
+console.log(fib(10)) // 55
+console.log(fib(20)) // 6765
+
+//next step: try the recursive version! this was another challenge given to me by my mentor
+function fibbing(n) {
+    if (n == 0) {
+        return 0
+    }
+    if (n == 1) {
+        return 1
+    }
+    return fibbing(n-1) + fibbing(n-2)
+}
+
+//more tests
+console.log(fibbing(0)) // 0
+console.log(fibbing(1)) // 1
+console.log(fibbing(10)) // 55
+console.log(fibbing(20)) // 6765
---
+++ isSorted.js
@@ -0,0 +1,9 @@
+//boolean based on if the numbers are ordered
+function isSorted(array) {
+    //is this how you represent an array?
+}
+
+//tests to check with for when I finish
+isSorted([]) // true
+isSorted([-Infinity, -5, 0, 3, 9]) // true
+isSorted([3, 9, -3, 10]) // false
